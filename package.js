22b387cbe1217589afef9871a29a65c6fa9e0332
koktic <sashalisov72@gmail.com>
2025-03-22 13:27:18 +0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "chat-server",
+  "version": "1.0.0",
+  "description": "WebSocket сервер для чата",
+  "main": "server.js",
+  "dependencies": {
+    "ws": "^8.13.0"
+  },
+  "scripts": {
+    "start": "node server.js"
+  },
+  "engines": {
+    "node": ">=18"
+  }
+}
